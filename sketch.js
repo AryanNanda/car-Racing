7831c10701e2e0f943ddae95597d838c138c502e
AryanNanda <65027409+AryanNanda@users.noreply.github.com>
2020-07-13 14:55:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas , backImg , gameState = 0,playerCount,database , form,player,game,allPlayers;
+function setup(){
+    canvas = createCanvas(400,400);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+function draw(){
+if (playerCount===4){
+    game.update(1);
+}
+if(gameState===1){
+clear();
+game.play();
+}
+}
